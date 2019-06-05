ca05e39c77b08c12ae3c71b33ccfa401c2aa482d
as-zhuravlev <duffyduckas@gmail.com>
2019-06-05 21:52:25 +0300

    fix fakeco2driver -> remoteco2driver usage

--- src/CO2Monitor.Controller/Startup.cs
+++ src/CO2Monitor.Controller/Startup.cs
@@ -45,7 +45,7 @@ namespace CO2Monitor.Controller
 
             services.AddScoped<IMeasurementRepository, SQLiteMeasurmentRepository>();
 
-            services.AddTransient<IRemoteCO2Driver, FakeRemoteCO2Driver>();
+            services.AddTransient<IRemoteCO2Driver, RemoteCO2Driver>();
 
             services.AddTransient<IRemoteCO2FanController, RemoteCO2FanController>();
 
